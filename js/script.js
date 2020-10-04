faf85e04b14e04464724eaa54f7df45373490bf2
mdimon313 <72306406+mdimon313@users.noreply.github.com>
2020-10-04 11:39:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var getStart = document.querySelector("#getStartBtn");
+var getTuch = document.querySelector("#gettouchBtn");
+var subscribe = document.querySelector("#subsBtn");
+
+function getStartedBtn() {
+	getStart.innerHTML="Thanks for Click";
+}
+
+function getTuchBtn() {
+ 	getTuch.innerHTML="Thanks for Tuch";
+}
+
+function subscribeBtn() {
+	subscribe.innerHTML = "Subscribed"
+}
